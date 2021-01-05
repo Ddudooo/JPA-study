20b7549a8a069946640d64ae6cc9e6fac1e7b999
Ddudooo <igoa.work@gmail.com>
2021-01-05 23:39:08 +0900

    영속성 전이, 고아 객체

--- jpa-proxy/src/main/java/com/jpastudy/mapping/Application.java
+++ jpa-proxy/src/main/java/com/jpastudy/mapping/Application.java
@@ -1,7 +1,7 @@
 package com.jpastudy.mapping;
 
-import com.jpastudy.mapping.entity.Member;
-import com.jpastudy.mapping.entity.Team;
+import com.jpastudy.mapping.entity.Child;
+import com.jpastudy.mapping.entity.Parent;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,20 +24,24 @@ public class Application {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member("hello");
-            Team team = new Team("team");
-            em.persist(team);
-            member.setTeam(team);
-            em.persist(member);
+            Parent parent = new Parent();
+
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.getReference(Member.class, member.getId());
-            System.out.println("m = "+ findMember.getTeam().getClass());
-            System.out.println("==============");
-            findMember.getTeam().getName();
-            System.out.println("==============");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
---
+++ jpa-proxy/src/main/java/com/jpastudy/mapping/entity/Parent.java
@@ -0,0 +1,35 @@
+package com.jpastudy.mapping.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * 영속성 전이 공부용.
+ */
+@Getter
+@Setter
+@Entity
+public class Parent {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Child> childList = new ArrayList<>();
+
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+}
