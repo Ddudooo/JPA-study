f4f1f032f515476865d4266a1d52a376010b3f54
Ddudooo <igoa.work@gmail.com>
2021-01-10 17:28:16 +0900

    경로 표현식과 패치조인 기본

--- jpa-jpql/src/main/java/com/jpastudy/mapping/Application.java
+++ jpa-jpql/src/main/java/com/jpastudy/mapping/Application.java
@@ -26,19 +26,39 @@ public class Application {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team("teamA");
-            em.persist(team);
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            Team teamC = new Team("teamC");
+
+            em.persist(teamA);
+            em.persist(teamB);
+            em.persist(teamC);
 
             Member member = new Member("member1");
             member.setAge(10);
-            member.changeTeam(team);
+            member.changeTeam(teamA);
             member.setType(MemberType.ADMIN);
             em.persist(member);
+
+            Member member2 = new Member("member2");
+            member2.setAge(10);
+            member2.changeTeam(teamA);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
+
+            Member member3 = new Member("member3");
+            member3.setAge(10);
+            member3.changeTeam(teamB);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join m.team t";
+            //fetch 조인
+            String query = "select m from Member m join fetch m.team";
+            //조인
+            //String query = "select m from Member m left join m.team t";
             //세터 조인
             //String query = "select m from Member m, Team t where m.name = t.name";
             //String query = "select m from Member m left join m.team t on t.name = 'teamA'";
@@ -53,6 +73,26 @@ public class Application {
                 .getResultList();
             for (Member findMember : memberList) {
                 System.out.println("member = " + findMember);
+                Team memberTeam = findMember.getTeam();
+                System.out.println("team = " + memberTeam.getName());
+            }
+
+            em.clear();
+            //일대다 fetch 조인의 경우 값이 n개
+            //team A 중복
+            //query = "select t from Team t join fetch t.members";
+
+            query = "select distinct t from Team t join fetch t.members";
+
+            List<Team> teamList = em
+                .createQuery(query, Team.class)
+                .getResultList();
+            for (Team findTeam : teamList) {
+                System.out.println("team = " + findTeam);
+                List<Member> findTeamMembers = findTeam.getMembers();
+                for(Member findMember : findTeamMembers) {
+                    System.out.println("member = " + findMember);
+                }
             }
 
             tx.commit();
